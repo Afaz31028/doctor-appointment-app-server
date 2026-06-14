5dc57972094b455f85036599aaf2ac24f4c11488
AfazurRahman <afazurr8@gmail.com>
2026-06-14 12:28:59 +0600

    primary setup completed

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const app = express();
+const dotenv= require('dotenv');
+dotenv.config();
+
+const cors = require('cors');
+const port= process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send('Server is Runnning Successfully');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on ${port} PORT`);
+})
